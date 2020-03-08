b44531bb0fd21ea4a06819cd11137feddae626d2
boomgreen87 <boomgreen87@yahoo.com>
2020-03-08 13:05:38 -0400

    New connection sound messed up MatchedID. Fixed Now

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ io.attach(server);
 
 io.on('connection', function(socket) {
     console.log('User connected');
-    socket.broadcast.emit('connected', { sID: `${socket.id}`, message: 'New Connection' });
+    socket.emit('connected', { sID: `${socket.id}`, message: 'New Connection' });
+    socket.broadcast.emit('new_user');
 
     // Listen for an incoming message from a user (socket refers to an individual user)
     // msg is the incoming message from that user
